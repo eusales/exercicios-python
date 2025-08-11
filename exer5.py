799f7cf8856a89998fdea680de20523acffae89a
Matheus Sales <matheus.sales@sou.ufmt.br>
2025-08-11 13:20:05 -0400

    uptade

--- exer5.py
+++ exer5.py
@@ -1,9 +1,18 @@
 """
+Exercício 5: Palíndromo
 Crie um programa que solicite ao usuário uma palavra e verifique se ela é um palíndromo. Um palíndromo é uma palavra que se lê da mesma forma de trás para frente.
 
 Exemplos:
 
 Palíndromos: arara, ovo, reviver, socos
+
+Não palíndromos: python, casa, computador
+
+Dicas:
+
+Lembre-se de remover espaços e considerar a palavra em letras minúsculas para evitar erros de comparação (ex: "A Torta" e "atorta").
+
+Uma maneira de inverter uma string em Python é usando o slicing de string: palavra[::-1].
 
 """
 while True:
---
+++ exer6.py
@@ -0,0 +1,17 @@
+"""
+Exercício 6: Encontrar o Anagrama Mais Longo
+Crie um programa que leia um arquivo de texto (.txt) contendo uma lista de palavras (uma por linha). O objetivo é encontrar o grupo de palavras que são anagramas umas das outras e que, juntas, formam o anagrama mais longo.
+
+Um anagrama é uma palavra formada pela reordenação das letras de outra palavra. Por exemplo, "amor" e "roma" são anagramas.
+
+O que o programa deve fazer:
+
+- Ler o arquivo de texto com a lista de palavras.
+
+- Agrupar as palavras que são anagramas. A maneira mais comum de fazer isso é criando uma "chave" para cada palavra. Por exemplo, você pode ordenar as letras de cada palavra ("amor" e "roma" se tornariam "amor") e usar essa string ordenada como chave para um dicionário.
+
+- Encontrar o grupo de anagramas mais longo. Ou seja, o grupo que contém o maior número de palavras.
+
+- Imprimir o resultado: O programa deve imprimir o grupo de palavras que formam o anagrama mais longo e a quantidade de palavras neste grupo.
+"""
+
